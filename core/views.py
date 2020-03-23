7a7b632037c55444242b34e02910664c1a1af46c
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-03-24 03:09:19 +0530

    Added India source

--- core/views.py
+++ core/views.py
@@ -48,6 +48,7 @@ def home_test(request):
 
 
 def sync(request):
+
     print("---------------------------------------------------")
     print("{}:Syncing records from web..".format(datetime.now()))
 
@@ -67,6 +68,9 @@ def sync(request):
     # Confirmed
     confirmed_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
     populateDb(stats_type='confirmed', url=confirmed_url)
+
+    # Populate India specific details
+    populateIndiaStats(url='https://api.rootnet.in/covid19-in/stats/daily')
 
     # Update summary table
     summary = updateSummaryTable()
@@ -123,8 +127,128 @@ def populateDb(url, stats_type):
     print("{}:Inserting records for stats_type[{}]..Done".format(datetime.now(), stats_type))
 
 
-def updateSummaryTable():
+def populateIndiaStats(url):
+
+    Record.objects.filter(country_region='India').delete()
     
+    # Globals
+    states_lat_long = {        
+        "India" : {"lat":20.5937, "long":78.9629},
+        "Andhra Pradesh" : {"lat":15.9129, "long":79.7400},
+        "Assam" : {"lat":26.244156, "long":92.537842},
+        "Bihar" : {"lat":25.0961, "long":85.3131},
+        "Chandigarh" : {"lat":30.7333, "long":76.7794},
+        "Chhattisgarh" : {"lat":21.295132, "long":81.828232},
+        "Delhi" : {"lat":28.7041, "long":77.1025},
+        "Gujarat" : {"lat":22.309425, "long":72.136230},
+        "Haryana" : {"lat":29.238478, "long":76.431885},
+        "Himachal Pradesh" : {"lat":32.084206, "long":77.571167},
+        "Jammu and Kashmir" : {"lat":33.7782, "long":76.5762},
+        "Karnataka" : {"lat":15.317277, "long":75.713890},
+        "Kerala" : {"lat":10.850516, "long":76.271080},
+        "Ladakh" : {"lat":34.152588, "long":77.577049},
+        "Madhya Pradesh" : {"lat":23.473324, "long":77.947998},
+        "Maharashtra" : {"lat":19.601194, "long":75.552979},
+        "Odisha" : {"lat":20.940920, "long":84.803467},
+        "Puducherry" : {"lat":11.9416, "long":79.8083},
+        "Punjab" : {"lat":31.1471, "long":75.3412},
+        "Rajasthan" : {"lat":27.391277, "long":73.432617},
+        "Tamil Nadu" : {"lat":11.127123, "long":78.656891},
+        "Telangana": {"lat":17.123184, "long":79.208824},
+        "Telengana": {"lat":17.123184, "long":79.208824},
+        "Tripura" : {"lat":23.745127, "long":91.746826},
+        "Uttar Pradesh" : {"lat":28.207609, "long":79.826660},
+        "Uttarakhand" : {"lat":30.0668, "long":79.0193},
+        "West Bengal" : {"lat":22.978624, "long":87.747803}
+    }
+
+    state_wise_stats = {}
+    # Fetch JSON data from url    
+    r = requests.get(url)
+    r_json = r.json()
+    for data in r_json['data']:
+        date = data['day']
+        date = datetime.strptime(date, "%Y-%m-%d").strftime("%y/%m/%d")
+        for regional in data['regional']:
+            state = regional['loc']
+            if(state in state_wise_stats.keys()):
+                pass
+            else:
+                state_wise_stats[state] = {}
+                state_wise_stats[state]['confirmed_csv'] = ''
+                state_wise_stats[state]['recovered_csv'] = ''
+                state_wise_stats[state]['deaths_csv'] = ''
+                state_wise_stats[state]['dates_csv'] = ''
+
+            # Handle the case where the state is not present in the states_lat_long dict
+            if (state in states_lat_long.keys()):
+                state_wise_stats[state]['lat'] = states_lat_long[state]['lat']
+                state_wise_stats[state]['long'] = states_lat_long[state]['long']
+            else:
+                state_wise_stats[state]['lat'] = states_lat_long['India']['lat']
+                state_wise_stats[state]['long'] = states_lat_long['India']['long']
+            state_wise_stats[state]['recovered_csv']    = state_wise_stats[state]['recovered_csv'] + str(regional['discharged']) + ","
+            state_wise_stats[state]['confirmed_csv']    = state_wise_stats[state]['confirmed_csv'] + str(regional['confirmedCasesIndian'] + regional['confirmedCasesForeign']) + ","
+            state_wise_stats[state]['deaths_csv']       = state_wise_stats[state]['deaths_csv'] + str(regional['deaths']) + ","
+            state_wise_stats[state]['dates_csv']        = state_wise_stats[state]['dates_csv'] + str(date) + ","            
+            state_wise_stats[state]['confirmed_latest'] = regional['confirmedCasesIndian'] + regional['confirmedCasesForeign']
+            state_wise_stats[state]['recovered_latest'] = regional['discharged']
+            state_wise_stats[state]['deaths_latest']    = regional['deaths']
+            state_wise_stats[state]['date_latest']      = str(date)
+    # At this time, we have collected all the data into the state_wise_stats dict
+    # Next step - Load data onto database using bulk insert
+    # Bulk insert requires array of objects to be created
+    objects_list = []
+    for state in state_wise_stats:
+        # For each state, we create 3 objects - 1.Confirmed 2.Recovered 3.Deaths        
+        obj = Record(
+            state_province     = state,
+            country_region     = 'India',
+            latitude           = state_wise_stats[state]['lat'],
+            longitude          = state_wise_stats[state]['long'],
+            stats_type         = 'confirmed',
+            latest_stats_date  = state_wise_stats[state]['date_latest'],
+            latest_stats_value = state_wise_stats[state]['confirmed_latest'],
+            stats_dates_csv    = state_wise_stats[state]['dates_csv'].rstrip(','),
+            stats_value_csv    = state_wise_stats[state]['confirmed_csv'].rstrip(','),
+        )
+        objects_list.append(obj)
+        obj = Record(
+            state_province     = state,
+            country_region     = 'India',
+            latitude           = state_wise_stats[state]['lat'],
+            longitude          = state_wise_stats[state]['long'],
+            stats_type         = 'deaths',
+            latest_stats_date  = state_wise_stats[state]['date_latest'],
+            latest_stats_value = state_wise_stats[state]['deaths_latest'],
+            stats_dates_csv    = state_wise_stats[state]['dates_csv'].rstrip(','),
+            stats_value_csv    = state_wise_stats[state]['deaths_csv'].rstrip(','),
+        )
+        objects_list.append(obj)        
+        obj = Record(
+            latitude           = state_wise_stats[state]['lat'],
+            longitude          = state_wise_stats[state]['long'],
+            stats_type         = 'recovered',
+            state_province     = state,
+            country_region     = 'India',
+            stats_dates_csv    = state_wise_stats[state]['dates_csv'].rstrip(','),
+            stats_value_csv    = state_wise_stats[state]['recovered_csv'].rstrip(','),
+            latest_stats_date  = state_wise_stats[state]['date_latest'],
+            latest_stats_value = state_wise_stats[state]['recovered_latest']
+        )
+        objects_list.append(obj)
+    
+    print("{}:Inserting INDIA records..".format(datetime.now()))
+    Record.objects.bulk_create(objects_list)
+    print("{}:Inserting INDIA records..Done".format(datetime.now()))
+
+
+
+
+
+
+def updateSummaryTable():
+
     print("{}: Computing summary from records fetched..".format(datetime.now()))
     details = {}
     details['utc_dt'] = str(datetime.now(timezone.utc))
@@ -132,7 +256,7 @@ def updateSummaryTable():
     details['countries'] = findSumAcrossEachCountry()['countries']
     details['trend_deaths']    = findTrend(stats_type='deaths')
     details['trend_confirmed'] = findTrend(stats_type='confirmed')
-    details['trend_recovered'] = findTrend(stats_type='recovered')    
+    details['trend_recovered'] = findTrend(stats_type='recovered')
     details['countriesSorted_Deaths']    = findCountriesSorted(stats_type='deaths')
     details['countriesSorted_Recovered'] = findCountriesSorted(stats_type='recovered')
     details['countriesSorted_Confirmed'] = findCountriesSorted(stats_type='confirmed')
@@ -199,7 +323,7 @@ def findCountriesSorted(stats_type):
 
 
 def findTrend(stats_type):
-    # Find the trend by counting sum of deaths/confirmed/recovered 
+    # Find the trend by counting sum of deaths/confirmed/recovered
     # across ALL countries for EACH date
     records = Record.objects.all().filter(stats_type=stats_type).values('stats_value_csv')
     trend = []
@@ -213,4 +337,4 @@ def findTrend(stats_type):
                 trend[index] = trend[index] + int(value)
 
     return trend
-    
+
