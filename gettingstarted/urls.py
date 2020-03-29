69a0c5a6834f5bf1f6cdff3b80089a5be3b95b93
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-03-29 18:16:18 +0530

    ...

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,12 +10,10 @@ from maps.views   import show_map
 admin.autodiscover()
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path("",               home,       name="core_home"),
     path("test/",          home_test,  name="core_home_test"),
     path("sync/",          sync,       name="core_sync"),
     path("api/coronafeed", coronafeed, name="corona-feed"),
-
     path("maps/world",     show_map,   name="show_map")
 
 ]
