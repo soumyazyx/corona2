326d76d38ac85b50ae0ecda0b1821ddcc3edd371
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-04-12 03:48:36 +0530

    Performance improvement of datatables

--- core/views.py
+++ core/views.py
@@ -19,10 +19,10 @@ from django.http import HttpResponse, JsonResponse, response
 # Custom imports
 from core.models import Record, Summary
 from lib.sync.sync_utils import rectifyDateFormat
-
+from lib.common.console import print_info
 
 def sync(request):
-
+    
     print("{}:Syncing records from web..".format(datetime.now()))
 
     # Each time we sync(once a day), we truncate the table of all its record
@@ -323,26 +323,34 @@ def findTrend(stats_type):
 
 
 def home(request):
-
+    # return render(request, "index.html")
+    print_info("Processing starts..")
+    print_info("Fetching records from DB..")
     model_values = Record.objects.all().filter(stats_type='confirmed').values(
         'latitude',
         'longitude',
         'country_region',
         'latest_stats_value'
     )
+    print_info("Fetching records from DB..Done")
 
+    print_info("Fetching json response from corona api..")
     summary_feed = requests.get('https://coronazyx.herokuapp.com/api/coronafeed')
     summary_json = summary_feed.json()
+    print_info("Fetching json response from corona api..Done")
     
-    # Fetch geo-json data
-    print("{}: Fetching geojson data..".format(datetime.now()))
+    print_info("Fetching geo-json data..")
     geo_json_data = json.loads(open('datasets/GeoJsonWorldCountries.json').read())
-    print("{}: Fetching geojson data..Done".format(datetime.now()))
+    print_info("Fetching geo-json data..Done")    
 
-
+    # Populate data table rows
+    table_html = get_table_html(summary_json=summary_json, geo_json_data=geo_json_data)
+    
+    
     # We are directly manipulating geojson to add in confirmed/recovered/deaths 
     # We need to manipulate the geojson as it is the one which choropleth consumes
     # Must be a better approach available - TBD
+    print_info("Manipulating geo-json data..")
     for obj in geo_json_data['features']:
         country_en     = obj['properties']['name'] # Afghanistan
         country_alpha2 = obj['id']                 # AFG
@@ -355,7 +363,9 @@ def home(request):
             # which implies - these are the countries wherein covid has not been reported
             # No further action is needed
             pass
-            
+    print_info("Manipulating geo-json data..Done")
+
+    print_info("beep..")
     countries_df = get_country_dataframes()
 
     beep = "Dummy|0"
@@ -408,11 +418,17 @@ def home(request):
         )
     ).add_to(m)
     map_html = m.get_root().render()
+
+
+
     context = {
         'map_html': map_html,
+        'table_html': table_html,
         "data": list(model_values),
         "summary": summary_feed.json()
     }
+    print_info("beep..Done")
+    print_info("returning...")
     return render(request, "index.html", context)
 
 
@@ -427,3 +443,48 @@ def get_country_dataframes( ):
     return countries_df
 
 
+def get_table_html(summary_json, geo_json_data):
+
+    # Create a local simplified dict from topojson - like so
+    # cntry = { "AFG": "Afghanistan", "ALB": "Albania", "DZA": "Algeria", "AND": "Andorra", "AGO": "Angola", "ATG": "Antigua", "ARG": "Argentina", "ARM": "Armenia", "AUS": "Australia", "AUT": "Austria", "AZE": "Azerbaijan", "BHS": "Bahamas", "BHR": "Bahrain", "BGD": "Bangladesh", "BRB": "Barbados", "BLR": "Belarus", "BEL": "Belgium", "BLZ": "Belize", "BEN": "Benin", "BTN": "Bhutan", "BOL": "Bolivia", "BIH": "Bosnia", "BWA": "Botswana", "BRA": "Brazil", "BRN": "Brunei", "BGR": "Bulgaria", "BFA": "Burkina", "BDI": "Burundi", "CPV": "CaboVerde", "KHM": "Cambodia", "CMR": "Cameroon", "CAN": "Canada", "CAF": "Central African Republic", "TCD": "Chad", "CHL": "Chile", "CHN": "China", "COL": "Colombia", "COM": "Comoros", "COG": "Congo", "COD": "Congo", "CRI": "Costa Rica", "CIV": "Côte d'Ivoire", "HRV": "Croatia", "CUB": "Cuba", "CYP": "Cyprus", "CZE": "Czechia", "DNK": "Denmark", "DJI": "Djibouti", "DMA": "Dominica", "DOM": "Dominican Rep", "ECU": "Ecuador", "EGY": "Egypt", "SLV": "El Salvador", "GNQ": "Guinea", "ERI": "Eritrea", "EST": "Estonia", "SWZ": "Eswatini", "ETH": "Ethiopia", "FJI": "Fiji", "FIN": "Finland", "FRA": "France", "GAB": "Gabon", "GMB": "Gambia", "GEO": "Georgia", "DEU": "Germany", "GHA": "Ghana", "GRC": "Greece", "GRD": "Grenada", "GTM": "Guatemala", "GIN": "Guinea", "GNB": "Guinea Bissau", "GUY": "Guyana", "HTI": "Haiti", "HND": "Honduras", "HUN": "Hungary", "ISL": "Iceland", "IND": "India", "IDN": "Indonesia", "IRN": "Iran", "IRQ": "Iraq", "IRL": "Ireland", "ISR": "Israel", "ITA": "Italy", "JAM": "Jamaica", "JPN": "Japan", "JOR": "Jordan", "KAZ": "Kazakhstan", "KEN": "Kenya", "KIR": "Kiribati", "PRK": "S Korea", "KOR": "N Korea", "KWT": "Kuwait", "KGZ": "Kyrgyzstan", "LAO": "Lao", "LVA": "Latvia", "LBN": "Lebanon", "LSO": "Lesotho", "LBR": "Liberia", "LBY": "Libya", "LIE": "Liechten stein", "LTU": "Lithuania", "LUX": "Luxembourg", "MDG": "Madagascar", "MWI": "Malawi", "MYS": "Malaysia", "MDV": "Maldives", "MLI": "Mali", "MLT": "Malta", "MHL": "Marshall Islands", "MRT": "Mauritania", "MUS": "Mauritius", "MEX": "Mexico", "FSM": "Micronesia", "MDA": "Moldova", "MCO": "Monaco", "MNG": "Mongolia", "MNE": "Montenegro", "MAR": "Morocco", "MOZ": "Mozambique", "MMR": "Myanmar", "NAM": "Namibia", "NRU": "Nauru", "NPL": "Nepal", "NLD": "Nether lands", "NZL": "New Zealand", "NIC": "Nicaragua", "NER": "Niger", "NGA": "Nigeria", "MKD": "North Macedonia", "NOR": "Norway", "OMN": "Oman", "PAK": "Pakistan", "PLW": "Palau", "PAN": "Panama", "PNG": "Papua New Guinea", "PRY": "Paraguay", "PER": "Peru", "PHL": "Philippines", "POL": "Poland", "PRT": "Portugal", "QAT": "Qatar", "ROU": "Romania", "RUS": "Russian", "RWA": "Rwanda", "KNA": "Saint Kitts and Nevis", "LCA": "Saint Lucia", "VCT": "Saint Vincent and the Grenadines", "WSM": "Samoa", "SMR": "San Marino", "STP": "Sao Tome and Principe", "SAU": "Saudi Arabia", "SEN": "Senegal", "SRB": "Serbia", "SYC": "Seychelles", "SLE": "Sierra Leone", "SGP": "Singapore", "SVK": "Slovakia", "SVN": "Slovenia", "SLB": "Solomon", "SOM": "Somalia", "ZAF": "South Africa", "SSD": "South Sudan", "ESP": "Spain", "LKA": "Sri Lanka", "SDN": "Sudan", "SUR": "Suriname", "SWE": "Sweden", "CHE": "Switzer land", "SYR": "Syria", "TJK": "Tajikistan", "TZA": "Tanzania", "THA": "Thailand", "TLS": "Timor Leste", "TGO": "Togo", "TON": "Tonga", "TTO": "Trinidad and Tobago", "TUN": "Tunisia", "TUR": "Turkey", "TKM": "Turkmeni stan", "TUV": "Tuvalu", "UGA": "Uganda", "UKR": "Ukraine", "ARE": "UAE", "GBR": "United Kingdom", "USA": "USA", "URY": "Uruguay", "UZB": "Uzbekistan", "VUT": "Vanuatu", "VEN": "Venezuela", "VNM": "Viet Nam", "YEM": "Yemen", "ZMB": "Zambia", "ZWE": "Zimbabwe" }
+    cntry = {}
+    for temp in geo_json_data['features']:
+        cntry[temp['id']] = temp['properties']['name']
+    
+                    
+
+
+    html = ''
+    for alpha3 in summary_json['countriesSorted_Confirmed']:
+        if alpha3 == '---':
+            continue
+        if alpha3 in cntry:
+            country = cntry[alpha3]
+        else:
+            country = alpha3
+
+        confirmed = summary_json['countries'][alpha3]['confirmed']
+        recovered = summary_json['countries'][alpha3]['recovered']
+        deaths    = summary_json['countries'][alpha3]['deaths']
+
+        country_href = f"<a href='/country/{alpha3}'>{country}</a>"
+        html += f"<tr>"
+        html += f"<td>{country_href}</td>"
+        html += f"<td class='text-right text-warning'>{confirmed}</td>"
+        html += f"<td class='text-right text-success'>{recovered}</td>"
+        html += f"<td class='text-right text-danger'>{deaths}</td>" 
+        html += f"</tr>"
+    
+    table_html  = f'\
+        <table id="table-count" class="table table-sm" style="width: 100%;">\
+            <thead>\
+            <tr>\
+                <td></td>\
+                <td class="text-right text-warning">Confirmed</td>\
+                <td class="text-right text-success">Recovered</td>\
+                <td class="text-right text-danger">Deaths</td>\
+            </tr>\
+            </thead>\
+            <tbody>{html}</tbody>\
+        </table>'
+    return table_html
